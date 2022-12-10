ec7e8fc301f7850fcdd7a6450f9e2f176191aeae
dryluigi <marioprasetyamulya85@gmail.com>
2022-12-10 19:59:04 +0700

    feat(cors): add cors header

--- BE/application.py
+++ BE/application.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from flask_cors import CORS
 from extensions.routes_extension import register_routes
 
 
 def create_app():
     app = Flask(__name__)
+    CORS(app)
 
     register_routes(app)
 
