2544b4ca817a497797a76bf9878add52c65c9e92
dryluigi <marioprasetyamulya85@gmail.com>
2023-07-05 21:26:42 +0700

    feat(prediction): change prediction page

--- FE/src/components/Footer/Footer.tsx
+++ FE/src/components/Footer/Footer.tsx
@@ -4,7 +4,7 @@ import { Box, Typography } from '@mui/material';
 function Footer() {
     return (
         <Box mt={8}>
-            <Typography align='center'>Application by <strong>Mario Prasetya Mulya</strong> | PENS | 2022</Typography>
+            <Typography align='center'>Application by <strong>Mario Prasetya Mulya</strong> | PENS | 2023</Typography>
         </Box>
     )
 }
--- FE/src/components/ScoreResult/ScoreResult.tsx
+++ FE/src/components/ScoreResult/ScoreResult.tsx
@@ -1,5 +1,18 @@
 import { Box, Stack, Typography } from '@mui/material';
 
+function getPalleteByScore(score: number) {
+    switch (score) {
+        case 1:
+            return { backgroundColor: 'rgba(52, 158, 235, .5)', color: 'black', };
+        case 2:
+            return { backgroundColor: 'rgba(255, 255, 0, .4)', color: 'black', };
+        case 3:
+            return { backgroundColor: 'rgba(255, 0, 0, .4)', color: 'black', };
+        default:
+            return { backgroundColor: 'rgba(0, 255, 0, .4)', color: 'black', };
+    }
+}
+
 interface ScorePair {
     left: number;
     right: number;
@@ -28,26 +41,78 @@ function ScoreResult(props: ScoreResultProps) {
                 >
                     <tbody>
                         <tr>
-                            <td style={{ padding: '2rem' }}>{props.topScore.left}</td>
-                            <td style={{ padding: '2rem' }}>{props.topScore.right}</td>
+                            <td style={{ padding: '2rem', ...getPalleteByScore(props.topScore.left) }}>{props.topScore.left}</td>
+                            <td style={{ padding: '2rem', ...getPalleteByScore(props.topScore.right) }}>{props.topScore.right}</td>
                         </tr>
                         <tr>
-                            <td style={{ padding: '2rem' }}>{props.middleScore.left}</td>
-                            <td style={{ padding: '2rem' }}>{props.middleScore.right}</td>
+                            <td style={{ padding: '2rem', ...getPalleteByScore(props.middleScore.left) }}>{props.middleScore.left}</td>
+                            <td style={{ padding: '2rem', ...getPalleteByScore(props.middleScore.right) }}>{props.middleScore.right}</td>
                         </tr>
                         <tr>
-                            <td style={{ padding: '2rem' }}>{props.lowerScore.left}</td>
-                            <td style={{ padding: '2rem' }}>{props.lowerScore.right}</td>
+                            <td style={{ padding: '2rem', ...getPalleteByScore(props.lowerScore.left) }}>{props.lowerScore.left}</td>
+                            <td style={{ padding: '2rem', ...getPalleteByScore(props.lowerScore.right) }}>{props.lowerScore.right}</td>
                         </tr>
                     </tbody>
                 </table>
                 <Box>
                     <Typography variant="body1" mb={1}>Penjelasan:</Typography>
                     <Typography variant="body2" mb={1}>Skor-skor di kiri merepresentasikan tingkat keparahan pada tiap area toraks. Rentang skor adalah <strong>0 - 3</strong> dengan ketentuan,</Typography>
-                    <Typography variant="body2"><strong>0</strong> : tidak terdapat indikasi Pneumonia COVID-19.</Typography>
-                    <Typography variant="body2"><strong>1</strong> : terdapat bekas luka/goresan pada jaringan kulit paru-paru.</Typography>
-                    <Typography variant="body2"><strong>2</strong> : terdapat alveolus yang membengkak.</Typography>
-                    <Typography variant="body2"><strong>3</strong> : terdapat bekas luka/goresan dan alveolus yang membengkak. Alveolus yang membekak lebih dominan..</Typography>
+                    <Typography variant="body2">
+                        <Box
+                            component={'span'}
+                            sx={{
+                                display: 'inline-block',
+                                height: '9px',
+                                width: '9px',
+                                backgroundColor: 'rgba(0, 255, 0, .4)',
+                                border: '1px solid black'
+                            }}
+                        >
+                        </Box> &nbsp;
+                        <strong>0</strong> : tidak terdapat indikasi Pneumonia COVID-19.
+                    </Typography>
+                    <Typography variant="body2">
+                        <Box
+                            component={'span'}
+                            sx={{
+                                display: 'inline-block',
+                                height: '9px',
+                                width: '9px',
+                                backgroundColor: 'rgba(52, 158, 235, .5)',
+                                border: '1px solid black'
+                            }}
+                        >
+                        </Box> &nbsp;
+                        <strong>1</strong> : terdapat bekas luka/goresan pada jaringan kulit paru-paru.
+                    </Typography>
+                    <Typography variant="body2">
+                        <Box
+                            component={'span'}
+                            sx={{
+                                display: 'inline-block',
+                                height: '9px',
+                                width: '9px',
+                                backgroundColor: 'rgba(255, 255, 0, .4)',
+                                border: '1px solid black'
+                            }}
+                        >
+                        </Box> &nbsp;
+                        <strong>2</strong> : terdapat alveolus yang membengkak.
+                    </Typography>
+                    <Typography variant="body2">
+                        <Box
+                            component={'span'}
+                            sx={{
+                                display: 'inline-block',
+                                height: '9px',
+                                width: '9px',
+                                backgroundColor: 'rgba(255, 0, 0, .4)',
+                                border: '1px solid black'
+                            }}
+                        >
+                        </Box> &nbsp;
+                        <strong>3</strong> : terdapat bekas luka/goresan dan alveolus yang membengkak. Alveolus yang membekak lebih dominan.
+                    </Typography>
 
                 </Box>
             </Stack>
